f14fbc4a477fe73d9b3791eed79a58434abfa9b4
andy ccs <andychong2@gmail.com>
2014-12-17 00:23:07 +0800

    Basic view now returns minimal HTML

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -9,3 +11,12 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	# Can we make this view function return some HTML which 
+	# will get the functional test to pass?
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>',response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
