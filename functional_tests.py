702adfa105b1c05807232908b0cfac07ec96a0fa
andy ccs <andychong2@gmail.com>
2014-12-16 21:39:12 +0800

    First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,33 +1,50 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
-browser.get('http://localhost:8000')
+		# waiting for page to complete loading before it tries to do anything,
+		# but it's not perfect. 
+		self.browser.implicitly_wait(3)
+		
+	def tearDown(self):
+		self.browser.quit()
+		
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get('http://localhost:8000')
 
-# She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do',self.browser.title)
 
-# She is invited to enter a to-do item straight away
+		# It is used as a reminder to finish the test
+		self.fail('Finish the test!')
 
-# She types "Buy peacok feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+		# She is invited to enter a to-do item straight away
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do lists
+		# She types "Buy peacok feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures)
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edutg us very methodical)
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do lists
 
-# The page updates again, and now shows both items on her lists
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edutg us very methodical)
 
-# Edith wonders whether the site will remember her list. The she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+		# The page updates again, and now shows both items on her lists
 
-# She visits the URL - her to-do list is still there.
+		# Edith wonders whether the site will remember her list. The she sees
+		# that the site has generated a unique URL for her -- there is some
+		# explanatory text to that effect.
 
-# Satisfied, she goes back to sleep
+		# She visits the URL - her to-do list is still there.
 
-browser.quit()
+		# Satisfied, she goes back to sleep
+
+# That's how Python script checks if it's been executed from the command line
+# , rather than just imported by another script
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
