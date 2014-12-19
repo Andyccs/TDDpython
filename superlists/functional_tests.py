4131cc371a160fdb26e43a07bb5e95706e7ef252
andy ccs <andychong2@gmail.com>
2014-12-19 11:22:55 +0800

    Front page HTML now generated from template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -26,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do',header_text)
 
 		# She is invited to enter a to-do item straight away
-		inputbox = self.browser.find_elemet_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
@@ -40,10 +40,11 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy peacock feathers" as an item in a to-do lists
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_elemet_by_id('id_list_table')
+		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New item did not appear in the table"
 			)
 
 		# There is still a text box inviting her to add another item. She
