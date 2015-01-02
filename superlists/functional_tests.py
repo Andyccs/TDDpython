62fc12a35dbcf2d3417eab2fd9a125c107aaddf7
Andyccs <y4andy@gmail.com>
2015-01-02 10:30:13 +0800

    refactor functional test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
 		
 	def tearDown(self):
 		self.browser.quit()
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
@@ -39,10 +44,7 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do lists
 		inputbox.send_keys(Keys.ENTER)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edutg is very methodical)
@@ -51,15 +53,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers',
-			[row.text for row in rows]
-			)
-		self.assertIn('2: Use peacock feathers to make a fly',
-			[row.text for row in rows]
-			)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith wonders whether the site will remember her list. The she sees
 		# that the site has generated a unique URL for her -- there is some
