cb8b35b3df9ccd788b259a66455c7e7f07ef0786
Andyccs <y4andy@gmail.com>
2015-01-02 10:22:30 +0800

    can handle one list item now, but not two

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -42,20 +42,29 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New item did not appear in the table"
-			)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 		# There is still a text box inviting her to add another item. She
-		# enters "Use peacock feathers to make a fly" (Edutg us very methodical)
-		self.fail('Finish the test!')
+		# enters "Use peacock feathers to make a fly" (Edutg is very methodical)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
-		# The page updates again, and now shows both items on her lists
+		# The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers',
+			[row.text for row in rows]
+			)
+		self.assertIn('2: Use peacock feathers to make a fly',
+			[row.text for row in rows]
+			)
 
 		# Edith wonders whether the site will remember her list. The she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
+		self.fail('Finish the test!')
 
 		# She visits the URL - her to-do list is still there.
 
